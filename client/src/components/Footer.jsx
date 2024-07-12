c3ee64fc1c3fef8db076a30ecf96b059e4da53eb
SanuliGehara <geharasanuli5@gmail.com>
2024-07-12 23:09:26 +0530

    create and add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,97 @@
+import { Footer } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from "react-icons/bs";
+
+const FooterCom = () => {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            {/* Logo */}
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Sanuli's
+              </span>
+              Blog
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  100 JS Projects
+                </Footer.Link>
+
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Sanuli's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+
+            <div>
+              <Footer.Title title="Follow Us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/SanuliGehara"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+
+                <Footer.Link
+                  href="https://www.linkedin.com/in/sanuli-gehara-71210b295/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Privacy Policy
+                </Footer.Link>
+
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
+                  Terms
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Sanuli's Blog"
+            year={new Date().getFullYear()}
+          />
+
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon
+              href="https://github.com/SanuliGehara"
+              icon={BsGithub}
+            />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default FooterCom;
