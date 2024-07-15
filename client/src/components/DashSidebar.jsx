d4d7cbd8d529acefbf1a8e7168f75893252a4588
SanuliGehara <geharasanuli5@gmail.com>
2024-07-15 19:47:22 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParam = new URLSearchParams(location.search);
+    const tabFromUrl = urlParam.get("tab");
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParam = new URLSearchParams(location.search);
+    const tabFromUrl = urlParam.get("tab");
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar*/}
+        <DashSidebar />
+      </div>
+      {/* Profile... */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
 
 export default Dashboard;
