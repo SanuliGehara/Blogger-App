bfb0b7117a8a2111cfb0b33148e93ccc8de43c28
SanuliGehara <geharasanuli5@gmail.com>
2024-07-16 13:43:52 +0530

    complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -25,6 +25,7 @@ export default function DashSidebar() {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
