6f2c33f334720ffb69573bbbe2cb8eccbe9dba9d
SanuliGehara <geharasanuli5@gmail.com>
2024-07-21 15:37:49 +0530

    show user posts inside the dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -34,10 +34,10 @@ export const getPosts = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
     const posts = await Post.find({
-      ...(req.query.userId && { userId: req.query.user }),
+      ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
-      ...(req.query.slug && { slug: req.query.slug }),
-      ...(req.query.postId && { postId: req.query.postId }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
           { title: { $regex: req.query.searchTerm, $options: "i" } },
@@ -45,7 +45,7 @@ export const getPosts = async (req, res, next) => {
         ],
       }),
     })
-      .sort({ updateAt: sortDirection })
+      .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
