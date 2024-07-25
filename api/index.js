6f3c7903d6ac81df6d6907ed6bf2a085cb0ba190
SanuliGehara <geharasanuli5@gmail.com>
2024-07-25 22:12:54 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("MonogoDB is connected"))
   .catch((error) => console.log(error));
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -27,6 +30,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Error handling
 app.use((err, req, res, next) => {
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import CallToAction from "../components/CallToAction";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen  flex items-center justify-center p-10">
+      <div className="max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-10 bg-purple-50 rounded-xl">
+        <h1 className="text-3xl font-semibold">Projects</h1>
+        <p className="text-md text-gray-500">
+          Build fun and engaging projects while learning HTML, CSS, and
+          JavaScript. You can learn and improve your knowledge on other
+          programming languages like Python and Java!
+        </p>
+        <CallToAction />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
