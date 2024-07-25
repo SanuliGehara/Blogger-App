1a6d7e85934842c600792c432da18213ad44af8d
SanuliGehara <geharasanuli5@gmail.com>
2024-07-25 22:12:54 +0530

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 function About() {
-  return <div>About us</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-10 text-center bg-purple-50 rounded-xl">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Sanuli's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Sanuli's Blog! This blog was created by Sanuli Gehara
+              as a personal project to share his thoughts and ideas with the
+              world. Sanuli is a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Sanuli is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
