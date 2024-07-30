5517d4be07f92e544ad30901002dfa11366cc672
SanuliGehara <geharasanuli5@gmail.com>
2024-07-30 10:24:21 +0530

    update sign in page ui

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -122,7 +122,7 @@ function Signin() {
 
             {/* Sign in option */}
             <div className="flex gap-2 text-sm mt-5">
-              <span>Don't have an account?</span>
+              <span>Dont have an account?</span>
               <Link to="/sign-up" className="text-blue-500">
                 Sign Up
               </Link>
